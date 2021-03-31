22462c3024480cb30d24a646174c7ab49219a7fd
nurgnc <gencbedriyenur@gmail.com>
2021-03-31 13:09:52 +0300

    created cart structure and line graph.

---
+++ src/components/CovidSummary.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from './Card'
+
+const CovidSummary = () => {
+    return (
+        <div>
+            <div>
+                <h1>World Wide Corona Report</h1>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+
+                    <Card>
+                        <span>Total Confirmed</span> <br />
+                        <span>0</span>
+                    </Card>
+                    <Card>
+                        <span>Total Recovered</span> <br />
+                        <span>0</span>
+                    </Card>
+                    <Card>
+                        <span>Total Deaths</span> <br />
+                        <span>0</span>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CovidSummary
